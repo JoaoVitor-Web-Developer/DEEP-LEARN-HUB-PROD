9905fe4de6b7dd47c97e910a976dd4c44c2ff3ff
João Vitor Targino De Oliveira <119077083+JoaoVitor-Web-Developer@users.noreply.github.com>
2023-11-23 13:59:45 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
-"use client"
 import { Button } from '@/components/ui/button';
+import { getAuthSession } from '@/lib/auth';
 import { signIn } from 'next-auth/react';
+import React from 'react';
 
-type Props = {}
+type Props = {};
 
-const Home = (props: Props) => {
+const Home: React.FC<Props> = async ({ ...authOptions }: Props) => {
+  const session = await getAuthSession(authOptions);
+
   return (
     <div className='flex flex-col items-center justify-center h-screen'>
       <h1 className='text-4xl font-bold mb-6'>
@@ -13,16 +16,32 @@ const Home = (props: Props) => {
       <p className='text-lg text-gray-600'>
         Transforme suas ideias em experiências educacionais incríveis com a nossa solução.
       </p>
-      <Button
-        className='mt-8'
-        onClick={() => {
-          signIn('google');
-        }}
-      >
-        Começar Agora
-      </Button>
+      {session ? (
+        <Button
+          className='mt-8'
+          onClick={() => {
+            window.location.href = '/create';
+          }}
+        >
+          Criar Curso
+        </Button>
+      ) : (
+        <div className="flex flex-col items-center justify-center h-screen">
+          <h1 className="text-4xl font-bold mb-6">
+            Realize o login para começar a gerar cursos
+          </h1>
+          <Button
+            className='mt-8'
+            onClick={() => {
+              signIn('google');
+            }}
+          >
+            Começar Agora
+          </Button>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default Home;
