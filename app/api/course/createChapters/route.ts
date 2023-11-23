230d8efa3c78022bec6d91b616957adbf75d106f
JoaoVitor-Web-Developer <joovitortgoliveira@gmail.com>
2023-11-23 11:02:12 -0300

    route fix bugs error

--- app/api/course/createChapters/route.ts
+++ app/api/course/createChapters/route.ts
@@ -9,9 +9,9 @@ import { prisma } from "@/lib/db";
 import { getAuthSession } from "@/lib/auth";
 import { checkSubscription } from "@/lib/subscription";
 
-export async function POST({...authOptions},req: Request, res: Response) {
+export async function POST(req: Request, res: Response) {
   try {
-    const session = await getAuthSession({authOptions});
+    const session = await getAuthSession();
     if (!session?.user) {
       return new NextResponse("Sem autorização", { status: 401 });
     }
